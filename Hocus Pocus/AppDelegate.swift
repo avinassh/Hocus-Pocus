beb101d5124e02cb4db93b53a6b70ec8ca7d0ffe
avi <avinashsajjan@gmail.com>
2015-03-30 20:56:49 +0530

    Initial Commit

---
+++ Hocus Pocus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Hocus Pocus
+//
+//  Created by avi on 30/03/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hocus PocusTests/Hocus_PocusTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hocus_PocusTests.swift
+//  Hocus PocusTests
+//
+//  Created by avi on 30/03/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Hocus_PocusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
