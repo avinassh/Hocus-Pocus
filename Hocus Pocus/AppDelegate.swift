89fa0df06bac1a238faf8a564f4e1c8ba662c785
avi <avinashsajjan@gmail.com>
2015-03-30 21:13:36 +0530

    added code for menuClicked(_:) completed and now app works as expected!

--- Hocus Pocus/AppDelegate.swift
+++ Hocus Pocus/AppDelegate.swift
@@ -46,7 +46,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @IBAction func showHiddenFiles(sender: NSMenuItem) {
-        println("Hidden files will be displayed")
+        // using NSTask we can send commands to terminal. It basically spawns
+        // a sub process and runs the task
+        let hideFilesTask = NSTask()
+        // path to the launchpath, which has the excuteable
+        hideFilesTask.launchPath = "/usr/bin/defaults"
+        
+        // Initially it's NSOffState, so for first time this if block will
+        // result to true and will be executed
+        if (sender.state == NSOffState) {
+            // lets change it to NSOnState and display hidden files
+            sender.state = NSOnState
+            hideFilesTask.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "YES"]
+        }
+        else {
+            sender.state = NSOffState
+            hideFilesTask.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "NO"]
+        }
+        
+        // run the command
+        hideFilesTask.launch()
+        // wait till the tasks are completed and then restart the finder
+        // to see effects
+        // This is blocking code
+        hideFilesTask.waitUntilExit()
+        
+        let killTask = NSTask()
+        killTask.launchPath = "/usr/bin/killall"
+        killTask.arguments = ["Finder"]
+        killTask.launch()
     }
     
 }
