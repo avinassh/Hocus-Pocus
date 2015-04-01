1942d98ccb540997dc88db0814e72fa052bd4ec0
avi <avinashsajjan@gmail.com>
2015-04-01 18:54:12 +0530

    Added a new project to main project, Hocus Pocus Helper. This app should help the main app to launch at login.

---
+++ Hocus Pocus Helper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Hocus Pocus Helper
+//
+//  Created by avi on 01/04/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
