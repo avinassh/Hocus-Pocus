d918e12bf4c5a82be72c55ffef00be0b645dc54a
avi <avinashsajjan@gmail.com>
2015-04-01 16:43:12 +0530

    added CCNPreferencesWindows library

---
+++ Hocus Pocus-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "CCNPreferencesWindowController.h"
+#import "CCNPreferencesWindowControllerProtocol.h"
